5cecf413332dc5e7800cf901e65769437111ea1a
alipov_cp <SND\alipov_cp>
2011-10-08 12:23:02 +0000

    Added Bootstrapper and Shell for ServerGui.

---
+++ Src/OpenRm.Server/OpenRm.Server.Gui/OpenRm.Server.Gui/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace OpenRm.Server.Gui
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
