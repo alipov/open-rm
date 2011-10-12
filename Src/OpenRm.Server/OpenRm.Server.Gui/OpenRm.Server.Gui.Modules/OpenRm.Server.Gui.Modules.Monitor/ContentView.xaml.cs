31937a4c93240745dfe5c40aec750b51c66deef3
Alex Lipov <3884590+alipov@users.noreply.github.com>
2011-10-12 20:58:27 +0000

    Reconstuction...

---
+++ Src/OpenRm.Server/OpenRm.Server.Gui/OpenRm.Server.Gui.Modules/OpenRm.Server.Gui.Modules.Monitor/ContentView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenRm.Server.Gui.Modules.Monitor
+{
+    /// <summary>
+    /// Interaction logic for Content.xaml
+    /// </summary>
+    public partial class ContentView : UserControl
+    {
+        public ContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
