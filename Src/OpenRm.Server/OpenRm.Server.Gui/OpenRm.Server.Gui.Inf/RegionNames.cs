ca96e947fbcc443a452db89ac0e4c85e703d9384
alipov_cp <SND\alipov_cp>
2011-10-12 20:58:27 +0000

    Reconstuction...

---
+++ Src/OpenRm.Server/OpenRm.Server.Gui/OpenRm.Server.Gui.Inf/RegionNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRm.Server.Gui.Inf
+{
+    public class RegionNames
+    {
+        public static string ToolbarRegion = "ToolbarRegion";
+        public static string ContentRegion = "ContentRegion";
+    }
+}
