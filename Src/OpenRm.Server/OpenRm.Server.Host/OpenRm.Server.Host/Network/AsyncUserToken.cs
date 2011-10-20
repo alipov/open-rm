8f70138c5c12e6c18ee085221f832bbe6c4ff328
mikas357_cp <SND\mikas357_cp>
2011-10-20 08:49:07 +0000

    TCP stack works with Micforost's  very BASIC client

--- Src/OpenRm.Server/OpenRm.Server.Host/OpenRm.Server.Host/Network/AsyncUserToken.cs
+++ Src/OpenRm.Server/OpenRm.Server.Host/OpenRm.Server.Host/Network/AsyncUserToken.cs
@@ -8,19 +8,17 @@ namespace OpenRm.Server.Host
 {
     class AsyncUserToken
     {
-        public Socket socket;
-        public ClientData data;
+        public Socket socket { get; set; }
+        public ClientData data { get; set; }
 
         // holds recieved message data (without prefix). used for storing partial messages also
-        public byte[] msgData;
-        public int recievedMsgPartLength;
+        public byte[] msgData { get; set; }
+        public int recievedMsgPartLength { get; set; }      // last enteres byte index
 
         // holds recieved prefix for cases when we get only a part of prefix, and need to call Receive method one more time
-        public byte[] prefixData;
-        public int recievedPrefixPartLength;
-
-        public int msgStartOffset;  
-        
+        public byte[] prefixData { get; set; }
+        public int recievedPrefixPartLength { get; set; }   // last enteres byte index
+        public int messageLength { get; set; }              // holds recieved prefix as Integer
 
         public AsyncUserToken() : this(null, null) { }
 
@@ -31,8 +29,17 @@ namespace OpenRm.Server.Host
             prefixData = new Byte[TCPServerListener.msgPrefixLength];       // 4 bytes prefix
             this.recievedPrefixPartLength = 0;
             this.recievedMsgPartLength = 0;
-            this.msgStartOffset = 0;
         }
+
+        // Prepare token for reuse
+        public void Clean()
+        {
+            msgData = null;
+            prefixData = new Byte[TCPServerListener.msgPrefixLength];
+            recievedMsgPartLength = 0;
+            recievedPrefixPartLength = 0;
+        }
+
 
 //TODO: can we remove it?
         public Socket Socket
