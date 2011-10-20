428b05c3e8330489b1e12f32f190bf28a4d805c6
Alex Lipov <3884590+alipov@users.noreply.github.com>
2011-10-20 15:24:57 +0000

    Added new project for Agent. Created basic program with NotifyIcon in place. Switched all the projects to target .NET Framework 4 Client Profile.

---
+++ Src/OpenRm.Agent/OpenRm.Agent.CustomControls/NotifyIconWrapper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using OpenRm.Agent.CustomControls.Views;
+
+namespace OpenRm.Agent.CustomControls
+{
+    public partial class NotifyIconWrapper : Component
+    {
+        // Use just one instance of this window.
+        private SettingsView _settingsView;
+        private Icon _greenIcon;
+        private Icon _redIcon;
+
+        public NotifyIconWrapper()
+        {
+            InitializeComponent();
+
+            InitializeResources();
+        }
+
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+
+            InitializeResources();
+        }
+
+        private void InitializeResources()
+        {
+            startAgentMenuItem.Click += StartAgentMenuItemClickEventHandler;
+            stopAgentMenuItem.Click += StopAgentMenuItemClickEventHandler;
+            settingsMenuItem.Click += SettingsMenuItemClickEventHandler;
+            quitMenuItem.Click += QuitMenuItemClickEventHandler;
+
+
+            _greenIcon = LoadIcon
+                ("/OpenRm.Agent.CustomControls;component/Images/GreenIcon.ico", "Failed to load Images/GreenIcon.ico");
+            _redIcon = LoadIcon
+                ("/OpenRm.Agent.CustomControls;component/Images/RedIcon.ico", "Failed to load Images/RedIcon.ico");
+
+            OpenRmNotifyIcon.Icon = _redIcon;
+        }
+
+        private Icon LoadIcon(string uriPath, string messageOnFail)
+        {
+            Icon result;
+            var iconUri = new Uri(uriPath, UriKind.RelativeOrAbsolute);
+            var resourceStream = Application.GetResourceStream(iconUri);
+            if (resourceStream == null)
+                throw new ApplicationException(messageOnFail);
+
+            using (Stream iconStream = resourceStream.Stream)
+            {
+                result = new Icon(iconStream);
+            }
+
+            return result;
+        }
+
+        private void StartAgentMenuItemClickEventHandler(object sender, EventArgs e)
+        {
+            startAgentMenuItem.Enabled = false;
+            stopAgentMenuItem.Enabled = true;
+            OpenRmNotifyIcon.Icon = _greenIcon;
+        }
+
+        private void StopAgentMenuItemClickEventHandler(object sender, EventArgs e)
+        {
+            stopAgentMenuItem.Enabled = false;
+            startAgentMenuItem.Enabled = true;
+            OpenRmNotifyIcon.Icon = _redIcon;
+        }
+
+        private void SettingsMenuItemClickEventHandler(object sender, EventArgs e)
+        {
+            if(_settingsView == null)
+                _settingsView = new SettingsView();
+
+            // Show the window (and bring it to the forefront if it's already visible).
+            if (_settingsView.WindowState == WindowState.Minimized)
+                _settingsView.WindowState = WindowState.Normal;
+            _settingsView.Show();
+            _settingsView.Activate();
+        }
+
+        private void QuitMenuItemClickEventHandler(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Src/OpenRm.Agent/OpenRm.Agent.CustomControls/Views/SettingsView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace OpenRm.Agent.CustomControls.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : Window
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+            Closing += WindowClosingEventHandler;
+        }
+
+        private void WindowClosingEventHandler(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            WindowState = WindowState.Minimized;
+            ShowInTaskbar = false;
+        }
+    }
+}
