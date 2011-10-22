4d42d0a0306859e5e917eb38316bf35f989fd0df
alipov_cp <SND\alipov_cp>
2011-10-22 14:04:03 +0000

    Made changes to Logger.

--- Src/OpenRm.Common/OpenRm.Common.Entities/Logger.cs
+++ Src/OpenRm.Common/OpenRm.Common.Entities/Logger.cs
@@ -1,52 +1,91 @@
 ﻿using System;
 using System.IO;
+using OpenRm.Common.Entities;
 
 namespace OpenRm.Server.Host
 {
-    public class Logger
+    public static class Logger //: IDisposable
     {
-        private string logDirectory = "logs";
-        private StreamWriter log;
+        // TODO: 1. in Main - read the log directory from app.config + call CreateLogDirectory method
+        //       2. each time we want to write log - execute the WriteStr method
 
-        private readonly object lck = new object();     // for handeling Writes from many threads
+        private static string _logFile; //= "asdf";
+        private static string _logDirectory = "";
+        //public static string logFilenamePattern; //= "server-<date>.log";
+        //private string logDirectory = "logs";
+        //private StreamWriter log;
 
-        public Logger(string logFilenamePattern)
-        {
-            //replace "<date>" by current date and time
-            string logFilename = logFilenamePattern.Replace("<date>", DateTime.Now.ToString("ddMMyy-HHmmss"));
-            try
-            {
-                CreateLogDirectory();       //check if Logs directory exist (otherwise creates it)
-                this.log = new StreamWriter(logDirectory + "\\" + logFilename);        //create and open file for writing
-            }
-            catch (IOException err)
-            {
-                //TODO:  show popup? ignore?
-                Console.WriteLine("ERROR while opening log file for writing: " + err.Message);
-            }
+        private static readonly object lck = new object();     // for handeling Writes from many threads
 
-        }
+        //public Logger(string logFilenamePattern)
+        //{
+        //    //replace "<date>" by current date and time
+        //    string logFilename = logFilenamePattern.Replace("<date>", DateTime.Now.ToString("ddMMyy-HHmmss"));
+        //    try
+        //    {
+        //        //CreateLogDirectory();       //check if Logs directory exist (otherwise creates it)
+        //        //this.log = new StreamWriter(logDirectory + "\\" + logFilename);        //create and open file for writing
+        //    }
+        //    catch (IOException err)
+        //    {
+        //        //throw new MyException(err); 
+        //        //TODO:  show popup? ignore?
+        //        Console.WriteLine("ERROR while opening log file for writing: " + err.Message);
+        //    }
 
-        private void CreateLogDirectory()
+        //}
+
+        public static void CreateLogFile(string logDirectory, string logPattern)
         {
             if (!Directory.Exists(logDirectory))
                 Directory.CreateDirectory(logDirectory);
+            //logFilenamePattern = logPattern;
+            _logDirectory = logDirectory;
+            _logFile = logPattern.Replace("<date>", DateTime.Now.ToString("ddMMyy-HHmmss"));
         }
 
-        public void WriteStr(string str)
+        public static void WriteStr(string str)
         {
-            lock (this.lck)
+            lock (lck)
             {
-                log.WriteLine(DateTime.Now.ToString("dd.MM HH:mm:ss") + " | " + str);
-                log.Flush();
+                using (var log = new StreamWriter(_logDirectory + "\\" + _logFile))
+                {
+                    log.WriteLine(DateTime.Now.ToString("dd.MM HH:mm:ss") + " | " + str);
+                    //log.Flush();
+                }
             }
         }
 
-        public void Close()
-        {
-            log.Close();
-        }
+        //public void Close()
+        //{
+        //    log.Close();
+        //}
 
 
+        //private bool _disposed;
+
+        //~Logger()
+        //{
+        //    Dispose(false);
+        //}
+
+        //public void Dispose()
+        //{
+        //    Dispose(true);
+        //    GC.SuppressFinalize(this);
+        //}
+
+        //private void Dispose(bool disposing)
+        //{
+        //    if (!_disposed)
+        //    {
+        //        if (disposing)
+        //        {
+
+        //        }
+        //        //log.Close();
+        //    }
+        //    _disposed = true;
+        //}
     }
 }
--- Src/OpenRm.Server/OpenRm.Server.Host/OpenRm.Server.Host/Server.cs
+++ Src/OpenRm.Server/OpenRm.Server.Host/OpenRm.Server.Host/Server.cs
@@ -8,33 +8,31 @@ namespace OpenRm.Server.Host
 {
     class Server
     {
-        private static string logFilenamePattern = "server-<date>.log";
-        private static Logger log;
+        private const string LogFilenamePattern = "server-<date>.log";
+        //private static Logger log;
 
-        public static int port = 3777;
-        public static int maxNumConnections = 1000;     //maximum number of connections
-        public static int receiveBufferSize = 16;      //recieve buffer size for tcp connection
+        public static int Port = 3777;
+        public static int MaxNumConnections = 1000;     //maximum number of connections
+        public static int ReceiveBufferSize = 16;      //recieve buffer size for tcp connection
         
         static void Main(string[] args)
         {
-            log = new Logger(logFilenamePattern);
-            log.WriteStr("Started");
+            //TODO: get directory from app.config
+            //System.Configuration.
+            Logger.CreateLogFile("logs", LogFilenamePattern);
+
+            //log = new Logger(logFilenamePattern);
+            Logger.WriteStr("Started");
+            //log.WriteStr("Started");
 
             try
             {
-                TCPServerListener srv = new TCPServerListener(port, maxNumConnections, receiveBufferSize, log);
+                TCPServerListener srv = new TCPServerListener(Port, MaxNumConnections, ReceiveBufferSize);
             }
             catch (Exception ex)
             {
-                log.WriteStr("ERROR: Exception: " + ex.Message + ". Trace: " + ex.StackTrace);
+                Logger.WriteStr("ERROR: Exception: " + ex.Message + ". Trace: " + ex.StackTrace);
             }
-
-
-
-            log.Close();
         }
-
-
-
     }
 }
