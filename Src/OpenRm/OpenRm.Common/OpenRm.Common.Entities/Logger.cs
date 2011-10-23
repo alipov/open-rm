2941cd7f776ac2cadef49ddffc46e177f41c1d27
mikas357_cp <SND\mikas357_cp>
2011-10-23 22:38:19 +0000

    fixed logger appending to file

--- Src/OpenRm/OpenRm.Common/OpenRm.Common.Entities/Logger.cs
+++ Src/OpenRm/OpenRm.Common/OpenRm.Common.Entities/Logger.cs
@@ -4,42 +4,20 @@ using OpenRm.Common.Entities;
 
 namespace OpenRm.Server.Host
 {
-    public static class Logger //: IDisposable
+    public static class Logger
     {
         // TODO: 1. in Main - read the log directory from app.config + call CreateLogDirectory method
         //       2. each time we want to write log - execute the WriteStr method
 
-        private static string _logFile; //= "asdf";
-        private static string _logDirectory = "";
-        //public static string logFilenamePattern; //= "server-<date>.log";
-        //private string logDirectory = "logs";
-        //private StreamWriter log;
+        private static string _logFile; 
+        private static string _logDirectory;
 
         private static readonly object lck = new object();     // for handeling Writes from many threads
-
-        //public Logger(string logFilenamePattern)
-        //{
-        //    //replace "<date>" by current date and time
-        //    string logFilename = logFilenamePattern.Replace("<date>", DateTime.Now.ToString("ddMMyy-HHmmss"));
-        //    try
-        //    {
-        //        //CreateLogDirectory();       //check if Logs directory exist (otherwise creates it)
-        //        //this.log = new StreamWriter(logDirectory + "\\" + logFilename);        //create and open file for writing
-        //    }
-        //    catch (IOException err)
-        //    {
-        //        //throw new MyException(err); 
-        //        //TODO:  show popup? ignore?
-        //        Console.WriteLine("ERROR while opening log file for writing: " + err.Message);
-        //    }
-
-        //}
 
         public static void CreateLogFile(string logDirectory, string logPattern)
         {
             if (!Directory.Exists(logDirectory))
                 Directory.CreateDirectory(logDirectory);
-            //logFilenamePattern = logPattern;
             _logDirectory = logDirectory;
             _logFile = logPattern.Replace("<date>", DateTime.Now.ToString("ddMMyy-HHmmss"));
         }
@@ -48,44 +26,12 @@ namespace OpenRm.Server.Host
         {
             lock (lck)
             {
-                using (var log = new StreamWriter(_logDirectory + "\\" + _logFile))
+                using (var log = new StreamWriter(_logDirectory + "\\" + _logFile, true))
                 {
                     log.WriteLine(DateTime.Now.ToString("dd.MM HH:mm:ss") + " | " + str);
-                    //log.Flush();
                 }
             }
         }
 
-        //public void Close()
-        //{
-        //    log.Close();
-        //}
-
-
-        //private bool _disposed;
-
-        //~Logger()
-        //{
-        //    Dispose(false);
-        //}
-
-        //public void Dispose()
-        //{
-        //    Dispose(true);
-        //    GC.SuppressFinalize(this);
-        //}
-
-        //private void Dispose(bool disposing)
-        //{
-        //    if (!_disposed)
-        //    {
-        //        if (disposing)
-        //        {
-
-        //        }
-        //        //log.Close();
-        //    }
-        //    _disposed = true;
-        //}
     }
 }
