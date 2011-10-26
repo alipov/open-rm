4c7cabe4be261bb3f3cc2436234f69a7a67cfdc3
mikas357_cp <SND\mikas357_cp>
2011-10-26 22:56:14 +0000

    Added reading configuration parameters from app.config, Adding event to EventLog in case of problem reading config file

--- Src/OpenRm/OpenRm.Agent/OpenRm.Agent/App.xaml.cs
+++ Src/OpenRm/OpenRm.Agent/OpenRm.Agent/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Windows;
 using OpenRm.Agent.CustomControls;
 using OpenRm.Common.Entities;
+using System.Configuration;
 
 namespace OpenRm.Agent
 {
@@ -11,9 +12,12 @@ namespace OpenRm.Agent
     /// </summary>
     public partial class App : Application
     {
-
-        private const string LogFilenamePattern = "client-<date>.log";
         public static bool agentStarted = false;            // flag that indicates current status of agent. can be changed by pressing "Stop Agent" control
+        private string serverIP;
+        private int serverPort;
+        private string logFilenamePattern;
+        public static int ReceiveBufferSize = 64;      //recieve buffer size for tcp connection
+
 
         private NotifyIconWrapper _notifyIconComponent;
 
@@ -27,16 +31,40 @@ namespace OpenRm.Agent
 
         private void StartAgent(object sender, EventArgs e)
         {
-            Logger.CreateLogFile("logs", LogFilenamePattern);
-            Logger.WriteStr("Client started.");
-            agentStarted = true;
+            if (ReadConfigFile())
+            {
 
-            // TODO: read configuration from config file 
-            TCPclient client = new TCPclient("127.0.0.1", 3777, 64);
+                Logger.CreateLogFile("logs", logFilenamePattern);       // creates "logs" directory in binaries folder and set log filename
+                Logger.WriteStr("Client started.");
+                agentStarted = true;
 
-            Logger.WriteStr("Client terminated");
+                TCPclient client = new TCPclient(serverIP, serverPort, ReceiveBufferSize);
+
+                Logger.WriteStr("Client terminated");
+            }
 
         }
+
+
+        // read configuration from config file
+        private bool ReadConfigFile()
+        {
+            try
+            {
+                 
+                serverIP = ConfigurationManager.AppSettings["ServerIP"];
+                serverPort = Int32.Parse(ConfigurationManager.AppSettings["ServerPort"]);
+                logFilenamePattern = ConfigurationManager.AppSettings["LogFilePattern"];
+            }
+            catch (Exception ex)
+            {
+                Logger.CriticalToEventLog("Error while reading config file. Error: " + ex.Message);
+                return false;
+            }
+
+            return true;   
+        }
+
 
         protected override void OnExit(ExitEventArgs e)
         {
--- Src/OpenRm/OpenRm.Common/OpenRm.Common.Entities/Logger.cs
+++ Src/OpenRm/OpenRm.Common/OpenRm.Common.Entities/Logger.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.IO;
 using OpenRm.Common.Entities;
+using System.Diagnostics;
 
 namespace OpenRm.Common.Entities
 {
     public static class Logger
     {
-        // TODO: 1. in Main - read the log directory from app.config + call CreateLogDirectory method
-        //       2. each time we want to write log - execute the WriteStr method
 
         private static string _logFile; 
         private static string _logDirectory;
@@ -19,7 +18,7 @@ namespace OpenRm.Common.Entities
             if (!Directory.Exists(logDirectory))
                 Directory.CreateDirectory(logDirectory);
             _logDirectory = logDirectory;
-            _logFile = logPattern.Replace("<date>", DateTime.Now.ToString("ddMMyy-HHmmss"));
+            _logFile = logPattern.Replace("[date]", DateTime.Now.ToString("ddMMyy-HHmmss"));
         }
 
         public static void WriteStr(string str)
@@ -38,6 +37,17 @@ namespace OpenRm.Common.Entities
                     Console.WriteLine("Error while writing to log: " + str + ". Exception: " + ex.ToString() + ". But we have to continue...");
                 }
             }
+        }
+
+        // writes critical event in Application EventLog
+        public static void CriticalToEventLog(string str)
+        {
+            string sSource = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+
+            if (!EventLog.SourceExists(sSource))
+                EventLog.CreateEventSource(sSource, "Application");
+
+            EventLog.WriteEntry(sSource, str, EventLogEntryType.Error);
         }
 
     }
