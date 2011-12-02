bdceae882c5a78810a1a8665b6f0517733228dd6
mikas357_cp <SND\mikas357_cp>
2011-12-02 20:48:33 +0000

    small fix

--- Src/OpenRm/OpenRm.Common/OpenRm.Common.Entities/Network/Messages/RemoveControlRequest.cs
+++ Src/OpenRm/OpenRm.Common/OpenRm.Common.Entities/Network/Messages/RemoveControlRequest.cs
@@ -12,5 +12,38 @@ namespace OpenRm.Common.Entities.Network.Messages
             ViewerIp = viewerIp;
             ViewerPort = viewerPort;
         }
+
+
+        public override ResponseBase ExecuteRequest()
+        {
+            // Start VNC Server
+            RunProcessRequest proc = new RunProcessRequest(
+                runId: 0,
+                cmd: "..\\Common\\ThirdParty\\VNC\\winvnc.exe",
+                args: "-run",
+                workDir: "..\\Common\\ThirdParty\\VNC\\",
+                delay: 0,
+                timeout: 1000,         //wait 1sec just to enshure that it has not failed
+                hidden: false);
+
+            var result = (RunProcessResponse) proc.ExecuteRequest();
+
+            if (result.ExitCode <= 0)
+            {
+                // connect to VNC listener
+                proc = new RunProcessRequest(
+                runId: 0,
+                cmd: "..\\Common\\ThirdParty\\VNC\\winvnc.exe",
+                args: "-connect " + ViewerIp + "::" + ViewerPort + " -shareall",
+                workDir: "..\\Common\\ThirdParty\\VNC\\",
+                delay: 0,
+                timeout: 1000,         //wait 1sec just to enshure that it has not failed
+                hidden: false);
+
+                result = (RunProcessResponse)proc.ExecuteRequest();
+            }
+
+            return new RemoteControlResponse(result.ExitCode, result.ErrorMessage);
+        }
     }
 }
