ad5730747de0337f11224e442191f008329b8272
mikas357_cp <SND\mikas357_cp>
2012-01-30 22:58:32 +0000

    Added locks (thread-safe) to server's _agent list manipulations. Message "AgentStatusUpdate" is sent on each agent status change. Please look how can we recieve it by Console, and update Agents List

---
+++ Src/OpenRm/OpenRm.Common/OpenRm.Common.Entities/Network/Messages/AgentStatusUpdate.cs
@@ -0,0 +1,11 @@
+﻿using OpenRm.Common.Entities.Enums;
+
+namespace OpenRm.Common.Entities.Network.Messages
+{
+    public class AgentStatusUpdate: ResponseBase
+    {
+        // Server sends status update to GUI on each agent status change.
+        public int status { get; set; }
+
+    }
+}
