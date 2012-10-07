28926770bd8c7c38320ab05a1685310a13bc8ae6
Alex Lipov <3884590+alipov@users.noreply.github.com>
2012-10-07 16:29:12 +0000

    + Added installer (fixes assembly blocking + creates folder structure, so drive root traversing for assembly file will be not achievable). + Started to work on Console's SplashScreen. + Replaced App.xaml with a custom class.

---
+++ Src/OpenRm/OpenRm.Server/OpenRm.Server.Gui/OpenRm.Server.Gui.App/App.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace OpenRm.Server.Gui
+{
+    public class App : Application
+    {
+        private const int MinimumSplashTime = 1500; // Miliseconds
+        private const int SplashFadeTime = 500;     // Miliseconds
+
+        // with help of: http://www.wpfsharp.com/2012/02/14/how-to-display-a-splash-screen-for-at-specific-minimum-length-of-time-in-wpf/
+        // deleting app.xaml: http://social.msdn.microsoft.com/Forums/pl/wpf/thread/0fdb3189-f51f-4a13-8217-1410335e5cd6
+        //                    http://www.codingconvention.com/tutorials/WPF
+
+        [STAThread()]
+        static void Main()
+        {
+            // Step 1 - Load the splash screen
+            //var splash = new SplashScreen("OpenRM.ico");
+            //splash.Show(false, true);
+
+            // Step 2 - Start a stop watch
+            var timer = new Stopwatch();
+            timer.Start();
+
+            // Step 3 - Load your windows but don't show it yet
+            TypeResolving.RegisterTypeResolving();
+            var app = new App();
+            app.Run();
+
+            // Step 4 - Make sure that the splash screen lasts at least two seconds
+            //timer.Stop();
+            //int remainingTimeToShowSplash = MinimumSplashTime - (int)timer.ElapsedMilliseconds;
+            //if (remainingTimeToShowSplash > 0)
+            //    Thread.Sleep(remainingTimeToShowSplash);
+
+            //// Step 5 - show the page
+            //splash.Close(TimeSpan.FromMilliseconds(SplashFadeTime));
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // create the bootstrapper
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
---
+++ Src/OpenRm/OpenRm.Server/OpenRm.Server.Gui/OpenRm.Server.Gui.App/SplashScreen/SplashScreenView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace OpenRm.Server.Gui.SplashScreen
+{
+    /// <summary>
+    /// Interaction logic for SplashScreenView.xaml
+    /// </summary>
+    public partial class SplashScreenView : Window
+    {
+        public SplashScreenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
